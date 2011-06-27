5965f548d6fcebe2fc70fac246e9bc00b0601d7e
Bryan McKelvey <bryan.mckelvey@gmail.com>
2011-06-27 14:58:41 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dokoe::Application.config.secret_token = 'f143f1a24e5d5504fb60fde25618040a72eccf65dafb903caf6593ebf9578cb50a30afbfb0015b313e33d0fb94c37fd5ff8580f4e9d42a6279a8b3e275dc288b'
